79b999f161e334113a6a599c674603c72b06d94b
KimrAndrew <andrewrk1997@gmail.com>
2022-01-10 21:02:29 -0800

    added create and delete views

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SnackListView, SnackDetailView
+from .views import SnackListView, SnackDetailView, SnackCreateView, SnackDeleteView
 
 urlpatterns = [
     path('',SnackListView.as_view(),name='snacks/snack-list'),
-    path('<int:pk>/',SnackDetailView.as_view(),name='snacks/snack-detail')
+    path('<int:pk>/',SnackDetailView.as_view(),name='snacks/snack-detail'),
+    path('create/',SnackCreateView.as_view(),name='snacks/snack-create'),
+    path('<int:pk>/delete/', SnackDeleteView.as_view(),name='snacks/snack-delete')
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls.base import reverse_lazy
 from django.views.generic import  ListView, CreateView, DetailView, DeleteView, UpdateView
 from .models import Snack
 # Create your views here.
@@ -9,4 +10,13 @@ class SnackListView(ListView):
 class SnackDetailView(DetailView):
     template_name='snacks/snack-detail.html'
     model = Snack
-    #fields = ['title','purchaser','description']
+
+class SnackCreateView(CreateView):
+    template_name = 'snacks/snack-create.html'
+    model = Snack
+    fields = ['title','purchaser','description']
+
+class SnackDeleteView(DeleteView):
+    template_name='snacks/snack-delete.html'
+    model = Snack
+    success_url = reverse_lazy('snacks/snack-list')
